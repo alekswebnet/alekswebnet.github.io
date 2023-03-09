ede824c0f11a10b3a414042c013cc937ef16a981
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2023-03-09 13:36:01 +0200

    Fix pdfjs-viewer-element demo

--- pdfjs-viewer-element/pdfjs-viewer-element.js
+++ pdfjs-viewer-element/pdfjs-viewer-element.js
@@ -1,41 +1,28 @@
 const r = document.createElement("template");
-r.innerHTML = `
-  <iframe
-    frameborder="0"
-    width="100%">
-  </iframe>
-  <style>
-    :host {
-      width: 100%;
-      display: block;
-    }
-    :host iframe {
-      height: inherit;
-    }
-  </style>
-`;
+r.innerHTML = '<iframe frameborder="0" width="100%"></iframe><style>:host{width:100%;display: block}:host iframe{height:inherit}</style>';
 class i extends HTMLElement {
-  static get observedAttributes() {
-    return ["src", "viewerPath"];
-  }
   constructor() {
     super(), this.attachShadow({ mode: "open" }).appendChild(r.content.cloneNode(!0));
   }
+  static get observedAttributes() {
+    return ["src", "viewer-path"];
+  }
   connectedCallback() {
-    this.hasAttribute("viewerPath") || this.setAttribute("viewerPath", "/pdfjs-3.4.120-dist"), this.updateIframeSrc();
+    this.hasAttribute("viewer-path") || this.setAttribute("viewer-path", "/pdfjs"), this.updateIframe();
   }
   attributeChangedCallback(e) {
-    ["src", "viewerPath"].includes(e) && this.updateIframeSrc();
+    ["src", "viewer-path"].includes(e) && this.updateIframe();
   }
-  updateIframeSrc() {
+  updateIframe() {
     var e, t;
     (t = (e = this.shadowRoot) == null ? void 0 : e.querySelector("iframe")) == null || t.setAttribute(
       "src",
-      `${this.getAttribute("viewerPath")}/web/viewer.html?file=${this.getAttribute("src") || ""}`
+      `${this.getAttribute("viewer-path")}/web/viewer.html?file=${this.getAttribute("src") || ""}`
     );
   }
 }
 window.customElements.get("pdfjs-viewer-element") || (window.PdfjsViewerElement = i, window.customElements.define("pdfjs-viewer-element", i));
 export {
+  i as PdfjsViewerElement,
   i as default
 };
