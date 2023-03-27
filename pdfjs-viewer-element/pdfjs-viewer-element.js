97c875913865367427058e98daa365225ac23dac
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2023-03-27 17:01:50 +0300

    Fix pdfjs-viewer-element docs

--- pdfjs-viewer-element/pdfjs-viewer-element.js
+++ pdfjs-viewer-element/pdfjs-viewer-element.js
@@ -1,46 +1,64 @@
-const d = (i, e = 300) => {
-  let t;
-  return function(...s) {
-    clearTimeout(t), t = setTimeout(() => i.apply(this, s), e);
-  };
-}, n = document.createElement("template");
-n.innerHTML = '<iframe frameborder="0" width="100%"></iframe><style>:host{width:100%;display:block;overflow:hidden}:host iframe{height:100%}</style>';
-class r extends HTMLElement {
+const w = (s, e) => new Promise((r, n) => {
+  let i = e.querySelector(s);
+  if (i) {
+    r(i);
+    return;
+  }
+  new MutationObserver((c, o) => {
+    Array.from(e.querySelectorAll(s)).forEach((a) => {
+      r(a), o.disconnect();
+    });
+  }).observe(e, {
+    childList: !0,
+    subtree: !0
+  });
+}), t = {
+  viewerPath: "/pdfjs",
+  viewerEntry: "/web/viewer.html",
+  src: "",
+  page: "",
+  search: "",
+  phrase: "",
+  zoom: "",
+  pagemode: "",
+  locale: "",
+  textLayer: ""
+}, m = document.createElement("template");
+m.innerHTML = `
+  <iframe frameborder="0" width="100%"></iframe>
+  <style>:host{width:100%;display:block;overflow:hidden}:host iframe{height:100%}</style>
+`;
+class d extends HTMLElement {
   constructor() {
-    super(), this.onAttrsChanged = d(async () => {
-      var s;
-      const t = (s = this.iframe) == null ? void 0 : s.getAttribute("src");
-      t && t.split("&locale=")[1] !== this.getAttribute("locale") ? (await this.setProps(), this.iframe.contentWindow.location.reload()) : await this.setProps();
-    }), this.attachShadow({ mode: "open" }).appendChild(n.content.cloneNode(!0));
+    super(), this.attachShadow({ mode: "open" }).appendChild(m.content.cloneNode(!0));
   }
   static get observedAttributes() {
-    return ["src", "viewer-path", "locale", "page", "search", "phrase", "zoom", "pagemode"];
+    return ["src", "viewer-path", "locale", "page", "search", "phrase", "zoom", "pagemode", "text-layer"];
   }
   connectedCallback() {
     this.iframe = this.shadowRoot.querySelector("iframe"), this.setEventListeners();
   }
   attributeChangedCallback() {
-    this.onAttrsChanged();
+    w("iframe", this.shadowRoot).then(() => this.setProps());
   }
-  async setProps() {
-    const e = this.getAttribute("viewer-path") || "/pdfjs", t = this.getFileSrc(this.getAttribute("src") || ""), s = this.getAttribute("page") || "", a = this.getAttribute("search") || "", c = this.getAttribute("phrase") || "", l = this.getAttribute("zoom") || "", h = this.getAttribute("pagemode") || "", o = this.getAttribute("locale");
-    this.iframe.setAttribute(
-      "src",
-      `${e}/web/viewer.html#file=${t}&page=${s}&zoom=${l}&pagemode=${h}&search=${a}&phrase=${c}${o ? "&locale=" + o : ""}`
-    );
+  setProps() {
+    const e = this.getFullPath(this.getAttribute("src") || t.src), r = this.getFullPath(this.getAttribute("viewer-path") || t.viewerPath), n = this.getAttribute("page") || t.page, i = this.getAttribute("search") || t.search, c = this.getAttribute("phrase") || t.phrase, o = this.getAttribute("zoom") || t.zoom, a = this.getAttribute("pagemode") || t.pagemode, h = this.getAttribute("locale") || t.locale, p = this.getAttribute("text-layer") || t.textLayer, l = `${r}${t.viewerEntry}?file=${e}#page=${n}&zoom=${o}&pagemode=${a}&search=${i}&phrase=${c}&textLayer=${p}${h ? "&locale=" + h : ""}`;
+    l !== this.iframe.getAttribute("src") && this.rerenderIframe(l);
+  }
+  rerenderIframe(e) {
+    this.shadowRoot.replaceChild(this.iframe.cloneNode(), this.iframe), this.iframe = this.shadowRoot.querySelector("iframe"), this.iframe.contentWindow.location.href = e;
   }
   setEventListeners() {
-    document.addEventListener("webviewerloaded", async () => {
-      var e, t;
-      (e = this.iframe.contentWindow) == null || e.PDFViewerApplicationOptions.set("disablePreferences", !0), (t = this.iframe.contentWindow) == null || t.PDFViewerApplicationOptions.set("pdfBugEnabled", !0);
+    document.addEventListener("webviewerloaded", () => {
+      this.getAttribute("src") !== t.src && this.iframe.contentWindow.PDFViewerApplicationOptions.set("defaultUrl", ""), this.iframe.contentWindow.PDFViewerApplicationOptions.set("disablePreferences", !0), this.iframe.contentWindow.PDFViewerApplicationOptions.set("pdfBugEnabled", !0);
     });
   }
-  getFileSrc(e) {
+  getFullPath(e) {
     return e.startsWith("/") ? `${window.location.origin}${e}` : e;
   }
 }
-window.customElements.get("pdfjs-viewer-element") || (window.PdfjsViewerElement = r, window.customElements.define("pdfjs-viewer-element", r));
+window.customElements.get("pdfjs-viewer-element") || (window.PdfjsViewerElement = d, window.customElements.define("pdfjs-viewer-element", d));
 export {
-  r as PdfjsViewerElement,
-  r as default
+  d as PdfjsViewerElement,
+  d as default
 };
